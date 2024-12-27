b7d84a17e7c8858a7168acaea22eb279f089b95c
Abhivanya <your_email@example.com>
2024-12-27 13:23:01 +0530

    added SGR on meetup detail page

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,8 +1,34 @@
 import { useRouter } from "next/router";
 import MeetupDetail from "../../components/meetups/MeetupDetail";
-const MeetupDetails = () => {
+const MeetupDetails = (props) => {
   const router = useRouter();
-  return <MeetupDetail meetupId={router.query.meetupId} />;
+  return <MeetupDetail meetupId={props.meetupId} />;
+};
+
+export function getStaticPaths() {
+  return {
+    fallback: true,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+export const getStaticProps = (context) => {
+  const meetupId = context.params.meetupId;
+  return {
+    props: {
+      meetupId: meetupId,
+    },
+  };
 };
 
 export default MeetupDetails;
